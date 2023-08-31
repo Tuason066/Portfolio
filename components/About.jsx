4533646613ec214f7790cb9d30c21acda0ad5765
Jeffrey Tuason <jepoyduque06@gmail.com>
2023-08-31 15:10:29 +0800

    Re-layout Project Section and update the about me description

--- components/About.jsx
+++ components/About.jsx
@@ -1,37 +1,44 @@
-"use client";
-
-import { social_links } from "@helper/constants";
-
-const Footer = () => {
+const About = () => {
   return (
-    <footer className="bg-dark text-lightest text-center py-8 md:py-12 lg:py-16">
-      <div className="center">
-        <div className="text-xl md:text-2xl lg:text-3xl align-horizontal gap-x-6 md:gap-x-8 lg:gap-x-10 justify-center">
-          {social_links.map(({ href, title, icon }) => (
-            <a
-              key={title}
-              href={href}
-              title={title}
-              target="_blank"
-              className="hover:opacity-70"
-            >
-              {icon}
-            </a>
-          ))}
-        </div>
-        <p className="mt-4 text-sm lg:text-base">
-          Developed and Designed by{" "}
+    <section
+      id="about"
+      className="bg-lightest text-dark text-center py-14 lg:py-20"
+    >
+      <div className="center max-w-4xl">
+        <h2 className="h2 uppercase text-darkest mb-8 lg:mb-16">about me</h2>
+        {/*  <p className="text-base md:text-lg lg:text-xl">
+          I am highly motivated self-taught{" "}
+          <strong>Front-end Web Developer</strong> with a passion for creating
+          pixel-perfect designs and seamless user experiences. Seeking a
+          position where I can leverage my skills in Nextjs, Nodejs, JavaScript
+          (ES6), and other front-end development tools to deliver high quality
+          web applications.
+        </p> */}
+        <p className="text-base md:text-lg lg:text-xl tracking-wide text-justify indent-4 md:indent-8">
+          I am a fourth-year College student at <em>Talisay City College</em>,
+          pursuing a{" "}
+          <em>
+            Bachelor of Science in Industrial Technology major in Computer
+            Technology
+          </em>
+          . Additionally, I am a{" "}
           <strong>
-            <a
-              href="https://www.facebook.com/Tuason06"
-              className="uppercase tracking-wider hover:underline"
-            >
-              jeffrey tuason
-            </a>
+            <em>self-taught front-end developer</em>
           </strong>
+          . I find joy in building web applications, and as time goes on, it has
+          transformed into a personal hobby. The tech stacks I have used include
+          Javascript(es6), nodejs, expressjs, nextjs, reactjs, tailwindcss, and
+          several other frontend development tools. What keeps me motivated in
+          web development is that I enjoy bringing my ideas to life, and what
+          makes it more interesting is that I can learn new things/skills every
+          day that are emerging frequently, as well as figuring out how to
+          implement a particular feature, optimize performance, or debug issues.
+          In the future, I might be able to find a work-from-home working
+          arrangement, which I prefer.
         </p>
       </div>
-    </footer>
+    </section>
   );
 };
-export default Footer;
+
+export default About;
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -8,9 +8,9 @@ import Masonry from "@mui/lab/Masonry";
 const Projects = () => {
   return (
     <section id="projects" className="bg-darkest py-14 lg:py-20">
-      <div data-aos="fade-right" className="center max-w-7xl">
+      <div className="center max-w-7xl">
         {/* TITLE */}
-        <h2 className="h2 uppercase mb-8 lg:mb-16">
+        <h2 data-aos="fade-right" className="h2 uppercase mb-8 lg:mb-16">
           <span className="relative text-lightest after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:bg-lightAccent">
             my
           </span>{" "}
@@ -23,7 +23,7 @@ const Projects = () => {
         <Masonry
           columns={{ xs: 1, md: 2, xl: 3 }}
           spacing={2}
-          className="max-w-[716px] xl:max-w-6xl mx-auto"
+          className="max-w-[716px] xl:max-w-4xl 2xl:max-w-6xl mx-auto"
         >
           {projects.map(
             ({
@@ -38,7 +38,11 @@ const Projects = () => {
                 data-aos-anchor-placement="top"
                 key={title}
               >
-                <figure className="bg-lightest rounded shadow max-w-[350px] mx-auto">
+                <figure
+                  key={title}
+                  // set the the max width to sustain the cover image
+                  className="bg-lightest rounded shadow max-w-[350px] mx-auto"
+                >
                   {/* IMAGE */}
                   <div className="h-full max-h-96 overflow-hidden rounded-t shadow mx-auto">
                     <Image
@@ -63,7 +67,7 @@ const Projects = () => {
                         href={live_url}
                         title="Preview"
                         target="_blank"
-                        className="border border-darkest rounded px-[1em] py-[.5em] capitalize hover:bg-darkest hover:text-lightest transition duration-500"
+                        className="border border-darkest text-darkest rounded px-[1em] py-[.5em] capitalize hover:bg-darkest hover:text-lightest transition duration-500"
                       >
                         live demo
                       </a>
@@ -76,7 +80,7 @@ const Projects = () => {
                         source code
                       </a>
                     </div>
-                    <div className="text-left">
+                    <div className="text-left text-dark">
                       {technologies.map((item, index) => (
                         <span key={index}>#{item} </span>
                       ))}
@@ -93,3 +97,46 @@ const Projects = () => {
 };
 
 export default Projects;
+
+// {/* <div data-aos="fade-up" data-aos-anchor-placement="top" key={title}>
+//   <figure className="bg-lightest rounded shadow max-w-[350px] mx-auto">
+//     {/* IMAGE */}
+//     <div className="h-full max-h-96 overflow-hidden rounded-t shadow mx-auto">
+//       <Image src={url} alt={title} width={width} height={height} />
+//     </div>
+//     {/* CONTENT */}
+//     <div className="text-center p-4 grid gap-y-4">
+//       <a
+//         href={live_url}
+//         title={"Project Title"}
+//         target="_blank"
+//         className="text-darkest text-lg font-semibold line-clamp-2"
+//       >
+//         <figcaption>{title}</figcaption>
+//       </a>
+//       <div className="flex gap-x-4 justify-center">
+//         <a
+//           href={live_url}
+//           title="Preview"
+//           target="_blank"
+//           className="border border-darkest rounded px-[1em] py-[.5em] capitalize hover:bg-darkest hover:text-lightest transition duration-500"
+//         >
+//           live demo
+//         </a>
+//         <a
+//           href={source_url}
+//           title="View Code"
+//           target="_blank"
+//           className="bg-darkest border border-darkest text-lightest  rounded px-[1em] py-[.5em] capitalize  hover:bg-dark hover:text-lightest transition duration-500"
+//         >
+//           source code
+//         </a>
+//       </div>
+//       <div className="text-left">
+//         {technologies.map((item, index) => (
+//           <span key={index}>#{item} </span>
+//         ))}
+//       </div>
+//     </div>
+//   </figure>
+// </div>; */}
