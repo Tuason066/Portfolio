662708195dd788ed2d18cb875d9178decfbceb00
jeffrey <jeffrey@geniihut.com>
2024-06-20 00:32:42 +0800

    update role and navlinks animation

--- app/layout.js
+++ app/layout.js
@@ -1,66 +1,70 @@
-import Footer from "@components/Footer";
-import Navbar from "@components/Navbar";
-import "../styles/globals.css";
-import ScrollTop from "@components/ScrollTop";
-import ScrollContext from "@context/ScrollContext";
-import HeightsContext from "@context/HeightsContext";
+import Footer from '@components/Footer';
+import Navbar from '@components/Navbar';
+import '../styles/globals.css';
+import ScrollTop from '@components/ScrollTop';
+import ScrollContext from '@context/ScrollContext';
+import HeightsContext from '@context/HeightsContext';
 
 export const metadata = {
-  title: "Jeffrey Tuason - Portfolio",
+  title: 'Jeffrey Tuason - Portfolio',
   description:
-    "I'm Jeffrey Tuason, a passionate front-end developer with expertise in creating modern and user-friendly web applications. Explore my portfolio to see my projects and skills.",
-  generator: "Next.js",
-  applicationName: "Jeffrey Tuason Portfolio",
-  url: "https://jeffrey-tuason-portfolio.netlify.app/",
-  siteName: "Jeffrey Tuason Portfolio",
+    "As a driven and innovative Software Engineer with a strong foundation in Computer Technology, I'm excited to bring my skills and passion to a forward-thinking team. With a keen eye for detail and a passion for crafting exceptional user experiences, I excel in creating visually stunning and highly functional web applications using cutting-edge technologies like Nextjs, Reactjs, and PHP. I'm now looking for a challenging role that will allow me to leverage my skills, expand my expertise, and contribute to the development of innovative solutions that make a real impact. Let's build something amazing together!",
+  generator: 'Next.js',
+  applicationName: 'Jeffrey Tuason Portfolio',
+  url: 'https://jeffrey-tuason-portfolio.netlify.app/',
+  siteName: 'Jeffrey Tuason Portfolio',
   keywords: [
-    "Front-End Developer",
-    "Web Developer",
-    "JavaScript",
-    "React",
-    "Portfolio",
-    "Nextjs",
-    "Nodejs",
-    "Expressjs",
-    "Java",
+    'Front-End Developer',
+    'Back-End Developer',
+    'Software Developer',
+    'Software Engineer',
+    'Web Developer',
+    'JavaScript',
+    'React',
+    'Portfolio',
+    'Nextjs',
+    'Nodejs',
+    'Expressjs',
+    'Java',
+    'PHP',
+    'Codeigniter',
   ],
   authors: [
     {
-      name: "Jeffrey Tuason",
-      url: "https://jeffrey-tuason-portfolio.netlify.app/",
+      name: 'Jeffrey Tuason',
+      url: 'https://jeffrey-tuason-portfolio.netlify.app/',
     },
   ],
-  colorScheme: "dark",
-  creator: "Jeffrey Tuason",
-  publisher: "Jeffrey Tuason",
+  colorScheme: 'dark',
+  creator: 'Jeffrey Tuason',
+  publisher: 'Jeffrey Tuason',
   formatDetection: {
-    email: "tuason.dev@email.com",
-    address: "Minglanilla, Cebu, Philippines",
-    telephone: "09456424506",
+    email: 'tuason.dev@email.com',
+    address: 'Minglanilla, Cebu, Philippines',
+    telephone: '09456424506',
   },
-  locale: "en_US",
-  type: "website",
+  locale: 'en_US',
+  type: 'website',
   openGraph: {
-    title: "Jeffrey Tuason - Portfolio",
-    description:
-      "Explore the portfolio of Jeffrey Tuason, a passionate front-end developer creating modern and user-friendly web applications.",
-    url: "https://jeffrey-tuason-portfolio.netlify.app/",
-    siteName: "Jeffrey Tuason Portfolio",
+    title: 'Jeffrey Tuason - Portfolio',
+    description: "As a driven and innovative Software Engineer with a strong foundation in Computer Technology, I'm excited to bring my skills and passion to a forward-thinking team. With a keen eye for detail and a passion for crafting exceptional user experiences, I excel in creating visually stunning and highly functional web applications using cutting-edge technologies like Nextjs, Reactjs, and PHP. I'm now looking for a challenging role that will allow me to leverage my skills, expand my expertise, and contribute to the development of innovative solutions that make a real impact. Let's build something amazing together!",
+    url: 'https://jeffrey-tuason-portfolio.netlify.app/',
+    siteName: 'Jeffrey Tuason Portfolio',
     images: [
       {
-        url: "/2x2,jpg",
+        url: '/2x2,jpg',
         width: 1200,
         height: 630,
       },
     ],
-    locale: "en_US",
-    type: "website",
+    locale: 'en_US',
+    type: 'website',
     images: [
       {
-        url: "/2x2.jpg",
+        url: '/2x2.jpg',
         width: 1200,
         height: 1200,
-        alt: "My Profile Picture",
+        alt: 'My Profile Picture',
       },
     ],
   },
@@ -68,8 +72,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className="font-lato light-theme">
+    <html lang='en'>
+      <body className='font-lato light-theme'>
         <ScrollContext>
           <HeightsContext>
             <Navbar />
--- helper/constants.js
+++ helper/constants.js
@@ -52,7 +52,7 @@ export const social_links = [
     icon: <Icon icon='akar-icons:github-fill' />,
   },
   {
-    href: 'mailto:jepoyduque06@gmail.com?subject=Front-end Developer Application from Portfolio',
+    href: 'mailto:jepoyduque06@gmail.com?subject=Software Engineer Application from Portfolio',
     title: 'Gmail',
     icon: <Icon icon='mdi:gmail' />,
   },
